fb3e2661800d38be6f918d152231d12f0160905d
elliot7williams <58864955+elliot7williams@users.noreply.github.com>
2026-08-01 22:04:40 -0400

    Add Learnables copy 3/LearnablesTests/LearnablesTests.swift

---
+++ Learnables copy 3/LearnablesTests/LearnablesTests.swift
@@ -0,0 +1,17 @@
+//
+//  LearnablesTests.swift
+//  LearnablesTests
+//
+//  Created by Elliot Williams on 2025-07-02.
+//
+
+import Testing
+@testable import Learnables
+
+struct LearnablesTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
